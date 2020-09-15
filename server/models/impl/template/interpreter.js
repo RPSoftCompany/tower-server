2feb15d56e9ec8a590340fabfa777f2aa48519c0
Radek <radekprzewuski@gmail.com>
2020-09-15 07:41:23 +0200

    assertion fix

--- server/models/impl/template/interpreter.js
+++ server/models/impl/template/interpreter.js
@@ -519,11 +519,11 @@ class If extends InterpreterCommon {
         }
 
         if (sign === '==') {
-            return `${varValue}` === `${condition}`;
+            return `${varValue}` == `${condition}`;
         } else if (sign === '>=') {
             return varValue >= condition;
         } else if (sign === '!=') {
-            return varValue != condition;
+            return `${varValue}` != `${condition}`;
         } else if (sign === '<=') {
             return varValue <= condition;
         } else if (sign === '>') {
